4233b790afae6a3f4cfcb5fe27129e52ff084ccc
kaylani <106694090+kaylanidocrime@users.noreply.github.com>
2023-11-21 13:17:10 -0300

    Update script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,47 +1,127 @@
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+var gamer, winner = null;
+var gamerSelected = document.getElementById('gamer-selected');
+var winnerSelected = document.getElementById('winner-selected');
 
-body {
-text-align: center;
-font-family: 'Roboto';
-background-color: #a1a8ab;
+changePlayer('X');
+
+function chooseSquare(id) {
+    if (winner !== null) {
+        return;
+    }
+
+    var square = document.getElementById(id);
+    if (square.innerHTML !== '-') {
+        return;
+    }
+
+    square.innerHTML = gamer;
+    square.style.color = '#000';
+
+    if (gamer === 'X') {
+        gamer = 'O';
+    } else {
+        gamer = 'X';
+    }
+
+    changePlayer(gamer);
+    checksWinner();
 }
 
-.square {
-text-align: center;
-width: 50px;
-background: #eee;
-display: inline-block;
-padding: 40px;
-font-size: 60px;
-margin: 5px;
-cursor: pointer;
-color: #eee;
+function changePlayer(valor) {
+    gamer = valor;
+    gamerSelected.innerHTML = gamer;
 }
 
-.gamer, .winner {
-font-size: 30px;
-margin-top: 10px;
+function checksWinner() {
+    var square1 = document.getElementById(1);
+    var square2 = document.getElementById(2);
+    var square3 = document.getElementById(3);
+    var square4 = document.getElementById(4);
+    var square5 = document.getElementById(5);
+    var square6 = document.getElementById(6);
+    var square7 = document.getElementById(7);
+    var square8 = document.getElementById(8);
+    var square9 = document.getElementById(9);
+    
+    if (checkSequence(square1, square2, square3)) {
+        changesColorSquare(square1, square2, square3);
+        changeWinner(square1);
+        return;
+    }
+
+    if (checkSequence(square4, square5, square6)) {
+        changesColorSquare(square4, square5, square6);
+        changeWinner(square4);
+        return;
+    }
+
+    if (checkSequence(square7, square8, square9)) {
+        changesColorSquare(square7, square8, square9);
+        changeWinner(square7);
+        return;
+    }
+
+    if (checkSequence(square1, square4, square7)) {
+        changesColorSquare(square1, square4, square7);
+        changeWinner(square1);
+        return;
+    }
+
+    if (checkSequence(square2, square5, square8)) {
+        changesColorSquare(square2, square5, square8);
+        changeWinner(square2);
+        return;
+    }
+
+    if (checkSequence(square3, square6, square9)) {
+        changesColorSquare(square3, square6, square9);
+        changeWinner(square3);
+        return;
+    }
+
+    if (checkSequence(square1, square5, square9)) {
+        changesColorSquare(square1, square5, square9);
+        changeWinner(square1);
+        return;
+    }
+
+    if (checkSequence(square3, square5, square7)) {
+        changesColorSquare(square3, square5, square7);
+        changeWinner(square3);
+    }
 }
 
-button {
-margin-top: 10px;
-width: 100px;
-height: 30px;
-background: #eee;
-
-cursor: pointer;
+function changeWinner(square) {
+    winner = square.innerHTML;
+    winnerSelected.innerHTML = winner;
 }
 
-footer {
-display: flex;
-flex-direction: column;
-align-items: center;
-text-align: center;
-color: #FFF;
-margin-top: 40px;
+function changesColorSquare(square1, square2, square3) {
+    square1.style.background = '#0f0';
+    square2.style.background = '#0f0';
+    square3.style.background = '#0f0';
 }
 
-footer a {
-text-decoration: none;
-color: #FFF;
+function checkSequence(square1, square2, square3) {
+    var eigual = false;
+
+    if (square1.innerHTML !== '-' && square1.innerHTML === square2.innerHTML && square2.innerHTML === square3.innerHTML) {
+        eigual = true;
+    }
+
+    return eigual;
+}
+
+function restart() {
+    winner = null;
+    winnerSelected.innerHTML = '';
+
+    for (var i = 1; i <= 9; i++) {
+        var square = document.getElementById(i);
+        square.style.background = '#eee';
+        square.style.color = '#eee';
+        square.innerHTML = '-';
+    }
+
+    changePlayer('X');
 }
